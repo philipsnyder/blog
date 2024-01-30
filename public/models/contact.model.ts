4946da51d643916ffe5e47df797a496141ef0023
philsnyder <philipsnyder@Philips-MacBook-Pro-2.local>
2024-01-29 21:27:56 -0700

    initial commit

---
+++ public/models/contact.model.ts
@@ -0,0 +1,10 @@
+export interface IContact {
+  firstName: string;
+  lastName: string;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+  phone: string;
+  birthday: string;
+}
---
+++ src/List.tsx
@@ -0,0 +1,7 @@
+// export async function loader () {
+//   await
+// }
+
+export default function List() {
+  return <div>The List!</div>;
+}
